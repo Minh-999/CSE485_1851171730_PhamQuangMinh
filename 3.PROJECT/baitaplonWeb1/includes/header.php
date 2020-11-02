fb6fcadc2b2b999910c514b6f5573ef31fec0024
Minh-999 <1851171730@e.tlu.edu.vn>
2020-11-02 09:36:31 +0700

    sua header.php

--- 3.PROJECT/baitaplonWeb1/includes/header.php
+++ 3.PROJECT/baitaplonWeb1/includes/header.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
   </head>
   <body>
   <div class="LOGO">
@@ -21,7 +22,7 @@
         </div>
     </div>
   <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
-  <a class="navbar-brand" href="trangchu.php">Trang chủ</a>
+  <a class="navbar-brand" href="trangchu.php"> <i class="fas fa-home"></i>  Trang chủ</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
