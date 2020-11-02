405345ed1c803b5ecd33d1e82b4d7228ea26e76e
Minh-999 <1851171730@e.tlu.edu.vn>
2020-11-02 17:14:35 +0700

    add

---
+++ 3.PROJECT/baitaplonWeb/cntt.php
@@ -0,0 +1,57 @@
+<?php
+include("includes/header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <div class="container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/pv3DAYZzLGE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    <h4>1. Mục tiêu đào tạo</h4>
+
+    Đào tạo kỹ sư có những kiến thức nền tảng trong lĩnh vực Công nghệ thông tin, cập nhật các vấn đề mới và hiện đại liên quan đến nghiên cứu phát triển các hệ thống thông tin, gia công hay ứng dụng hệ thống phần mềm; kiến thức về mạng máy tính và truyền thông.
+
+<h5>2. Chương trình đào tạo</h5>
+
+- Thời gian đào tạo: 4.5 năm với 145 Tín chỉ
+
+- Chương trình đào tạo được thiết kế hiện đại, cập nhật, kế thừa từ các trường danh tiếng trên thế giới như Michigan, Harvard của Hoa Kỳ. Nhiều môn học đáp ứng nhu cầu của xã hội, theo đơn đặt hàng của các doanh nghiệp như FPT, SamSung,...
+<h5>3. Nhu cầu lao động của ngành đào tạo</h5>
+
+- Theo thống kê của Bộ Thông tin - Truyền thông, đến năm 2020, nhân lực trong lĩnh vực CNTT mỗi năm tăng 13%. Theo đó, lĩnh vực CNTT ở Việt Nam cần đến 1 triệu lao động. Tuy nhiên hiện nay các trường mới chỉ đào tạo đáp ứng được gần một nửa.
+
+<h5>4. Nơi làm việc sau khi sinh viên tốt nghiệp</h5>
+
+<p>- Lập trình viên, kỹ sư phần mềm, nhà phân tích thiết kế phần mềm tại các công ty tập đoàn phát triển phần mềm
+
+- Giảng dạy và nghiên cứu ở các Trường, Viện, Trung tâm
+
+- Kỹ sư trưởng của các dự án phần mềm, nhà quản trị dự án phần mềm
+
+- Có nhiều cơ hội tham gia thị trường lao động CNTT toàn cầu.</p>
+
+<h5>5. Các điểm mạnh của Ngành đào tạo tại Trường ĐH Thủy Lợi</h5>
+
+<b>Sau khi tốt nghiệp, người học được cấp bằng Kỹ sư Công nghệ thông tin. Họ có thể đảm nhiệm các công việc:</b>
+Nắm vững một số ngôn ngữ, công cụ lập trình. Công cụ phát triển hệ thống.
+Có thể xây dựng, phát triển các hệ thống công nghệ thông tin ứng dụng trong thực tế.
+Nghiên cứu khoa học thuộc các lĩnh vực về công nghệ phần mềm, công nghệ thông tin…ở các viện nghiên cứu, các trung tâm và cơ quan nghiên cứu của các Bộ, ngành, các trường Đại học và Cao đẳng;
+Làm chuyên viên ở bộ phận công nghệ thông tin hoặc cần ứng dụng công nghệ thông tin của tất cả các đơn vị có nhu cầu (hành chính sự nghiệp, ngân hàng, viễn thông, hàng không, xây dựng, thủy lợi…);
+Làm việc trong các công ty sản xuất, gia công phần mềm trong nước cũng như nước ngoài;
+Làm việc tại các công ty tư vấn về đề xuất giải pháp, xây dựng và bảo trì các hệ thống thông tin, hệ thống mạng và truyền thông…
+- Giảng dạy các môn liên quan đến công nghệ thông tin tại các trường đại học, cao đẳng, trung học chuyên nghiệp, dạy nghề và các trường phổ thông;
+
+<h5>5. Các điểm mạnh của Ngành đào tạo tại Trường ĐH Thủy Lợi</h5>
+
+- Khoa CNTT có hệ thống phòng Lab và cơ sở vật chất hiện đại. Tạo điều kiện cho các SV có thể tham gia vào các nhóm nghiên cứu khoa học, các đề tài dự án thực tế.
+
+- Chú trọng tới kỹ năng thực tập, thực hành của sinh viên. Có hệ thống phòng học cơ sở vật chất và các phòng Lab khang trang hiện đại</p>
+  <h4>Chi tiết:</h4>
+  <img src="img/CNTTChuan.png" alt="">
+</body>
+</html>
---
+++ 3.PROJECT/baitaplonWeb/ktpm.php
@@ -0,0 +1,59 @@
+<?php
+include("includes/header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <div class="container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/RJ3MIAbSySQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    <h4>1. Mục tiêu đào tạo</h4>
+
+<p>Cung cấp các kiến thức chuyên sâu giúp phát triển, sử dụng và bảo trì phần mềm một cách có hệ thống, có kỷ luật. Giúp sinh viên ra trường có thể làm hòa nhập nhanh chóng với môi trường phát triển phần mềm chuyên nghiệp tại các công ty, tập đoàn phát triển phần mềm.</p>
+
+<h5>2. Chương trình đào tạo</h5>
+
+<p>- Thời gian đào tạo: 04 năm với 140 Tín chỉ
+
+- Nội dung chương trình: Chương trình đào tạo được thiết kế dựa trên việc nghiên cứu và kế thừa các chương trình đào tạo của các trường đại học hàng đầu trong nước và nước ngoài. Bao gồm nhiều môn học đáp ứng nhu cầu của xã hội, theo đơn đặt hàng của các doanh nghiệp như FPT, SamSung,... Nội dung cụ thể bao gồm:
+
++  Khối kiến thức Đại cương theo quy định của Bộ GDĐT.
+
++  Kiến thức căn bản về công nghệ thông tin.
+
++  Quy trình phát triển phần mềm, từ phương pháp, kỹ thuật, công nghệ trong phân tích, thiết kế, phát triển, kiểm thử, bảo trì phần mềm và quản lý dự án phần mềm.
+
++  Tiếng Anh đáp ứng nhu cầu đọc tài liệu chuyên ngành, giao tiếp, làm việc trong các tổ chức, công ty trong và ngoài nước.
+
++  Thực hành, làm các Project ứng dụng thực tế, thực tập tại các đơn vị, doanh nghiệp liên kết hợp tác với Khoa, Trường.
+</p>
+<h5>3. Nhu cầu lao động của ngành đào tạo</h5>
+
+<p>- Thị trường CNTT Việt Nam nổi tiếng chủ yếu về dịch vụ gia công phần mềm - outsourcing. Việt Nam hiện nằm trong top 10 nước hàng đầu tại châu Á - Thái Bình Dương và 30 của thế giới về gia công và phát triển phần mềm.
+
+- Theo VietnamWorks, đến năm 2020 Việt Nam sẽ thiếu hơn 500.000 nhân lực CNTT, trong đó Kỹ thuật phần mềm là ngành có nhu cầu nhân lực cao nhất. Việt Nam đang rất thiếu các chuyên gia CNTT giỏi chuyên môn, thông thạo ngoại ngữ và có kỹ năng làm việc chuyên nghiệp.</p>
+
+<h5>4. Nơi làm việc sau khi sinh viên tốt nghiệp</h5>
+
+<p>- Lập trình viên, kỹ sư phần mềm, nhà phân tích thiết kế phần mềm tại các công ty tập đoàn phát triển phần mềm
+
+- Giảng dạy và nghiên cứu ở các Trường, Viện, Trung tâm
+
+- Kỹ sư trưởng của các dự án phần mềm, nhà quản trị dự án phần mềm
+
+- Có nhiều cơ hội tham gia thị trường lao động CNTT toàn cầu.</p>
+
+<h5>5. Các điểm mạnh của Ngành đào tạo tại Trường ĐH Thủy Lợi</h5>
+
+<p>- Kỹ sư ngành Kỹ thuật phần mềm tại ĐH Thủy Lợi, có thời gian đào tạo chỉ 04 năm gồm 140 tín chỉ.
+
+- Chú trọng tới kỹ năng thực tập, thực hành của sinh viên. Có hệ thống phòng học cơ sở vật chất và các phòng Lab khang trang hiện đại</p>
+  <h4>Chi tiết:</h4>
+  <img src="img/KTPM.png" alt="">
+</body>
+</html>
