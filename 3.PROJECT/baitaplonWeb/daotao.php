804452512b9fcb475e53fdecb69dc9adaa308441
Minh-999 <1851171730@e.tlu.edu.vn>
2020-11-02 17:06:32 +0700

    them 1 dong

---
+++ 3.PROJECT/baitaplonWeb/daotao.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+      <?php
+  include("includes/header.php")
+  ?>
+   <h3>  Chương trình đào tạo</h3>
+       <div class="row">
+       <div class="col-md-4">
+       <div class="card" style="width: 30rem;">
+  <img src="img/cnpm.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Ngành kĩ thuật phần mềm</h5>
+    <p class="card-text">Cung cấp các kiến thức chuyên sâu giúp phát triển, sử dụng và bảo trì phần mềm một cách có hệ thống, có kỷ luật. Giúp sinh viên ra trường có thể làm hòa nhập nhanh chóng với môi trường phát triển phần mềm chuyên nghiệp tại các công ty, tập đoàn phát triển phần mềm.</p>
+    <a href="ktpm.php" class="btn btn-primary">Chi tiết</a>
+  </div>
+</div>
+<div class="card" style="width: 30rem;">
+  <img src="img/cntt.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Ngành Công nghệ thông tin</h5>
+    <p class="card-text">Đào tạo kỹ sư có những kiến thức nền tảng trong lĩnh vực Công nghệ thông tin, cập nhật các vấn đề mới và hiện đại liên quan đến nghiên cứu phát triển các hệ thống thông tin, gia công hay ứng dụng hệ thống phần mềm; kiến thức về mạng máy tính và truyền thông.</p>
+    <a href="cntt.php" class="btn btn-primary">Chi tiết</a>
+  </div>
+</div>
+<div class="card" style="width: 30rem;">
+  <img src="img/httt.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Ngành Hệ thống thông tin</h5>
+    <p class="card-text">Đào tạo kỹ sư có những kiến thức nền tảng trong lĩnh vực Hệ thống thông tin, cập nhật các vấn đề mới và hiện đại liên quan đến nghiên cứu phát triển các hệ thống thông tin, gia công hay ứng dụng hệ thống phần mềm; kiến thức về mạng máy tính và truyền thông; kiến thức về phân tích thiết kế khai thác cơ sở dữ liệu, phân tích xử lý dữ liệu lớn, xây dựng những hệ thống hỗ trợ việc ra quyết định.</p>
+    <a href="httt.php" class="btn btn-primary">Chi tiết</a>
+  </div>
+</div>
+       </div>
+       </div>
+       </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ 3.PROJECT/baitaplonWeb/httt.php
@@ -0,0 +1,59 @@
+<?php
+include("includes/header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <div class="container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/EFRYogGdIYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    <h4>1. Mục tiêu đào tạo</h4>
+
+<p>Đào tạo kỹ sư có những kiến thức nền tảng trong lĩnh vực Hệ thống thông tin, cập nhật các vấn đề mới và hiện đại liên quan đến nghiên cứu phát triển các hệ thống thông tin, gia công hay ứng dụng hệ thống phần mềm; kiến thức về mạng máy tính và truyền thông; kiến thức về phân tích thiết kế khai thác cơ sở dữ liệu, phân tích xử lý dữ liệu lớn, xây dựng những hệ thống hỗ trợ việc ra quyết định.</p>
+
+<h5>2. Chương trình đào tạo</h5>
+
+<p>- Thời gian đào tạo: 04 năm với 140 Tín chỉ
+
+- Chương trình đào tạo được xây dựng rất cập nhật hiện đại theo xu hướng mới của thế giới, nhiều môn học đáp ứng nhu cầu của xã hội, theo đơn đặt hàng của các doanh nghiệp như FPT, SamSung,...
+</p>
+<h5>3. Nhu cầu lao động của ngành đào tạo</h5>
+
+<p>Tại Việt Nam: đến năm 2020 cần đến một triệu nhân lực về CNTT & HTTT. Tuy nhiên hiện nay các trường mới chỉ đào tạo đáp ứng được gần một nửa.
+Trên thế giới: HTTT có lương khởi điểm đứng thứ 15/144 nghề nghiệp tại Mỹ. Theo (Harvard Business Review), Khoa học dữ liệu sẽ là một trong những ngành “thời thượng” của thế kỷ 21  -> Đó cũng chính là nhiệm vụ của ngành HTTT.</p>
+
+<h5>4. Nơi làm việc sau khi sinh viên tốt nghiệp</h5>
+
+<p> <b>Chuẩn đầu ra:</b>
+
+Có thể lập trình, nắm vững ít nhất các công cụ lập trình (C++, C#, Lập trình Web (PHP, .NET), Java, Lập trình di động,…)
+Có khả năng quản lý, phân tích dữ liệu và thiết kế các hệ thống thông tin phục vụ cho kinh tế xã hội, tài chính, nông nghiệp, công nghiệp, giao thông, an ninh quốc phòng,…
+Sử dụng thành thạo một số công cụ xử lý và phân tích Dữ liệu như (R, Matlab, Weka, SPSS, AMOS,…)
+Hiểu và xây dựng các ứng dụng Khai phá dữ liệu (Data mining), học máy (Machine learning), trí tuệ nhân tạo, mạng neural nhân tạo (neural network), nhận dạng, xử lý ảnh, điện toán đám mây,…
+Xây dựng được những hệ thống khuyến nghị sản phẩm, hệ hỗ trợ ra quyết định, hệ thống dự báo, chương trình trò chơi,…
+Có khả năng học tập nâng cao sau khi tốt nghiệp lên trình độ Thạc sĩ, Tiến sĩ tại các cơ sở đào tạo trong và ngoài nước.
+<h6>Vị trí việc làm sau khi tốt nghiệp:</h6>
+
+Chuyên viên phân tích dữ liệu, lập Dự án, hoạch định chính sách phát triển CNTT cho các tổ chức và doanh nghiệp.
+Chuyên viên xây dựng, quản trị, phát triển các hệ thống để vận hành doanh nghiệp, tổ chức như hệ thống ERP, SCM, CRM, HR ,…
+Lập trình viên trong các Công ty tập đoàn lớn như FPT, Viettel, Samsung… chuyên sản xuất, gia công phần mềm trong và ngoài nước.
+Có thể đi xuất khẩu lao động dưới dạng kỹ sư cho các thị trường nước ngoài như Nhật Bản, Hàn Quốc,…
+Quản lý dự án phần mềm, công nghệ thông tin, Giám đốc thông tin (CIO Chief of Information Officer).
+Cán bộ nghiên cứu ở các Viện, trung tâm, cơ quan nghiên cứu của các Bộ, Ngành, các trường đại học, cao đẳng. Giảng dạy CNTT tại trường Đại học, Cao đẳng, nghề và phổ thông.</p>
+
+<h5>5. Các điểm mạnh của Ngành đào tạo tại Trường ĐH Thủy Lợi</h5>
+
+<p>- - Ngành HTTT tại ĐH Thủy lợi có định hướng sâu về Khoa học dữ liệu.
+
+- Thời gian đào tạo linh hoạt chỉ 04 năm gồm 140 tín chỉ được cấp bằng kỹ sư HTTT.
+
+- Chú trọng tới kỹ năng thực tập, thực hành của sinh viên. Có hệ thống phòng học cơ sở vật chất và các phòng Lab khang trang hiện đại.</p>
+  <h4>Chi tiết:</h4>
+  <img src="img/HTTT_1.png" alt="">
+</body>
+</html>
