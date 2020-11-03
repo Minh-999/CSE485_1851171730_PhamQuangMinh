c0fbbe1a9deaf44b17c5aa1018e781bd68ea7f5e
Minh-999 <1851171730@e.tlu.edu.vn>
2020-11-03 21:22:52 +0700

    sua 1 ti

--- 3.PROJECT/baitaplonWeb/includes/header.php
+++ 3.PROJECT/baitaplonWeb/includes/header.php
@@ -21,7 +21,9 @@
     <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
     <a class="navbar-brand" href="trangchu.php"><i class="fas fa-home"></i>Trang chủ</a>
     <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-        aria-expanded="false" aria-label="Toggle navigation"></button>
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
     <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item active">
